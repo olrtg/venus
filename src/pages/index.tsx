ae8f4c4db2a137b3f758eb72ee24edb0c2883c17
José Olórtegui <20072509+olrtg@users.noreply.github.com>
2022-02-05 23:22:26 -0300

    added on demand icons support

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
+import IconRocket from '~icons/twemoji/rocket'
+
 const Index = () => (
-  <h1 className="text-2xl font-bold">Hello World!</h1>
+  <h1 className="text-2xl font-bold">Hello World! <IconRocket className="inline-block" /></h1>
 )
 
 export default Index
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 
 import react from '@vitejs/plugin-react'
 import Pages from 'vite-plugin-pages'
+import Icons from 'unplugin-icons/vite'
 
 export default defineConfig({
   resolve: {
@@ -10,5 +11,5 @@ export default defineConfig({
       '~/': `${path.resolve(__dirname, 'src')}/`,
     },
   },
-  plugins: [react(), Pages()],
+  plugins: [react(), Pages(), Icons({ compiler: 'jsx', jsx: 'react', autoInstall: true })],
 })
