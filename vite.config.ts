a5312b3ae1b521fbbc9a6e1d8869ac00b094efee
José Olórtegui <20072509+olrtg@users.noreply.github.com>
2022-02-06 20:44:53 -0300

    added PWA support

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 
 import react from '@vitejs/plugin-react'
 import Pages from 'vite-plugin-pages'
+import { VitePWA } from 'vite-plugin-pwa'
 import AutoImport from 'unplugin-auto-import/vite'
 import Icons from 'unplugin-icons/vite'
 import IconsResolver from 'unplugin-icons/resolver'
@@ -16,6 +17,33 @@ export default defineConfig({
   plugins: [
     react(),
     Pages(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.png', 'robots.txt', 'safari-pinned-tab.png'],
+      manifest: {
+        name: 'Venus',
+        short_name: 'Venus',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: '/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+    }),
     AutoImport({
       imports: [
         'react',
