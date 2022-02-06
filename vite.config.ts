2bcffe88e1d5dbaf79cfe50277db2096439d3d74
José Olórtegui <20072509+olrtg@users.noreply.github.com>
2022-02-06 03:28:00 -0300

    added auto import with support for react and react-router-dom

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 
 import react from '@vitejs/plugin-react'
 import Pages from 'vite-plugin-pages'
+import AutoImport from 'unplugin-auto-import/vite'
 import Icons from 'unplugin-icons/vite'
 
 export default defineConfig({
@@ -11,5 +12,16 @@ export default defineConfig({
       '~/': `${path.resolve(__dirname, 'src')}/`,
     },
   },
-  plugins: [react(), Pages(), Icons({ compiler: 'jsx', jsx: 'react', autoInstall: true })],
+  plugins: [
+    react(),
+    Pages(),
+    AutoImport({
+      imports: [
+        'react',
+        'react-router-dom',
+      ],
+      dts: 'src/auto-imports.d.ts',
+    }),
+    Icons({ compiler: 'jsx', jsx: 'react', autoInstall: true }),
+  ],
 })
