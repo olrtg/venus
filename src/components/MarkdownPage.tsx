aaca4508b7f39eeffa1a69f584ca062950288cb9
José Olórtegui <20072509+olrtg@users.noreply.github.com>
2022-02-08 21:02:20 -0300

    added mdx support

---
+++ src/components/MarkdownPage.tsx
@@ -0,0 +1,7 @@
+const MarkdownPage: React.FC = ({children}) => (
+  <main className="prose">
+    {children}
+  </main>
+)
+
+export default MarkdownPage
--- vite.config.ts
+++ vite.config.ts
@@ -8,55 +8,63 @@ import AutoImport from 'unplugin-auto-import/vite'
 import Icons from 'unplugin-icons/vite'
 import IconsResolver from 'unplugin-icons/resolver'
 
-export default defineConfig({
-  resolve: {
-    alias: {
-      '~/': `${path.resolve(__dirname, 'src')}/`,
-    },
-  },
-  plugins: [
-    react(),
-    Pages(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.png', 'robots.txt', 'safari-pinned-tab.png'],
-      manifest: {
-        name: 'Venus',
-        short_name: 'Venus',
-        theme_color: '#ffffff',
-        icons: [
-          {
-            src: '/pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: '/pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-          {
-            src: '/pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable',
-          },
-        ],
+export default defineConfig(async () => {
+  const mdx = await import('@mdx-js/rollup')
+
+  return { 
+    resolve: {
+      alias: {
+        '~/': `${path.resolve(__dirname, 'src')}/`,
+        'react/jsx-runtime': 'react/jsx-runtime.js'
       },
-    }),
-    AutoImport({
-      imports: [
-        'react',
-        'react-router-dom',
-      ],
-      dts: 'src/auto-imports.d.ts',
-      resolvers: [
-        IconsResolver({
-          prefix: 'Icon',
-          extension: 'jsx',
-        }),
-      ],
-    }),
-    Icons({ compiler: 'jsx', jsx: 'react', autoInstall: true }),
-  ],
+    },
+    plugins: [
+      react(),
+      Pages({
+        extensions: ['tsx', 'mdx']
+      }),
+      mdx.default(),
+      VitePWA({
+        registerType: 'autoUpdate',
+        includeAssets: ['favicon.png', 'robots.txt', 'safari-pinned-tab.png'],
+        manifest: {
+          name: 'Venus',
+          short_name: 'Venus',
+          theme_color: '#ffffff',
+          icons: [
+            {
+              src: '/pwa-192x192.png',
+              sizes: '192x192',
+              type: 'image/png',
+            },
+            {
+              src: '/pwa-512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+            },
+            {
+              src: '/pwa-512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+              purpose: 'any maskable',
+            },
+          ],
+        },
+      }),
+      AutoImport({
+        imports: [
+          'react',
+          'react-router-dom',
+        ],
+        dts: 'src/auto-imports.d.ts',
+        resolvers: [
+          IconsResolver({
+            prefix: 'Icon',
+            extension: 'jsx',
+          }),
+        ],
+      }),
+      Icons({ compiler: 'jsx', jsx: 'react', autoInstall: true }),
+    ],
+  }
 })
