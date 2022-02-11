83a1a00f56464a56faafbbd7201ea81b7c1fb6bd
José Olórtegui <20072509+olrtg@users.noreply.github.com>
2022-02-11 15:49:16 -0300

    404 page design

--- src/pages/[...all].tsx
+++ src/pages/[...all].tsx
@@ -1,5 +1,13 @@
-const All = () => (
-  <h1>Not found</h1>
-)
+function All() {
+  const { pathname } = useLocation()
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <h1 className="text-3xl font-bold"><code>404</code> - Not Found</h1>
+      <p className="mt-2">We couldn't find any routes that matches <code className="p-1 text-sm text-blue-600 rounded bg-gray-50">{pathname}</code>.</p>
+      <Link className="px-4 py-2 mt-4 text-white transition-colors bg-blue-600 rounded hover:bg-blue-500" to="/">Go to home</Link>
+    </div>
+  )
+}
 
 export default All
